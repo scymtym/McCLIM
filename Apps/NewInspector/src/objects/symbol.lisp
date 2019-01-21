fbc18230f0a6568b4dc8ec391b90145cafea1cc8
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-01-21 14:52:29 +0100

    new-inspector: symbol improvements

--- Apps/NewInspector/src/objects/symbol.lisp
+++ Apps/NewInspector/src/objects/symbol.lisp
@@ -17,10 +17,23 @@
 
 (cl:in-package #:new-inspector)
 
+;;; Utilities
+
+(declaim (inline package-locked?))
+(defun package-locked? (package)
+  #+sbcl (sb-ext:package-locked-p package)
+  #-sbcl nil)
+
 ;;; `symbol-slot-place'
 
 (defclass symbol-slot-place (basic-place) ; TODO make a base class without cell slot
   ())
+
+#+sbcl (defmethod supportsp :around ((place symbol-slot-place) (operation t))
+         (and (if-let ((package (symbol-package (container place))))
+                (not (package-locked? package))
+                t)
+              (call-next-method)))
 
 (defmethod supportsp ((place symbol-slot-place) (operation (eql 'remove-value)))
   t)
@@ -67,28 +80,80 @@
 (defmethod value ((place symbol-function-place))
   (symbol-function (container place)))
 
-(defmethod (setf value) ((new-value t) (place symbol-function-place))
+(defmethod (setf value) ((new-value function) (place symbol-function-place))
   (setf (symbol-function (container place)) new-value))
 
 (defmethod make-unbound ((place symbol-function-place))
   (fmakunbound (container place)))
 
+;;; `symbol-type-place'
+
+(defclass symbol-type-place (symbol-slot-place)
+  ())
+
+(defmethod accepts-value-p ((place symbol-type-place) (value t))
+  nil)
+
+(defmethod accepts-value-p ((place symbol-type-place) (value class))
+  t)
+
+(defmethod valuep ((place symbol-type-place))
+  (find-class (container place) nil))
+
+(defmethod value ((place symbol-type-place))
+  (find-class (container place) nil))
+
+(defmethod (setf value) ((new-value class) (place symbol-type-place))
+  (setf (find-class (container place) nil) new-value))
+
+(defmethod make-unbound ((place symbol-type-place))
+  (setf (find-class place nil) nil))
+
+;;; Object states
+
+(defclass inspected-package (inspected-object)
+  ((%symbol-filter :initarg  :symbol-filter
+                   :accessor symbol-filter
+                   :initform nil)))
+
+(defmethod (setf symbol-filter) :after ((new-value t)
+                                        (object    inspected-package))
+  ())
+
+(defmethod make-object-state ((object package) (place t))
+  (make-instance 'inspected-package :place place))
+
 ;;; Object inspection methods
+
+(defmethod inspect-object-using-state ((object symbol)
+                                       (state  inspected-object)
+                                       (style  (eql :expanded-header))
+                                       (stream t))
+  (call-next-method)
+  (write-char #\Space stream)
+  (let ((package (symbol-package object)))
+    (cond ((null package)
+           (badge stream "uninterned"))
+          (t
+           (with-output-as-badge (stream)
+             (let ((state (nth-value 1 (find-symbol (symbol-name object) package)))) ; TODO ugly
+               (write-string (string-downcase state) stream)))))))
 
 (defmethod inspect-object-using-state ((object symbol)
                                        (state  inspected-object)
                                        (style  (eql :expanded-body))
                                        (stream t))
+  (print-documentation object stream :namespace 'function) ; TODO not just function
+
   (formatting-table (stream)
     (formatting-row (stream)
       (formatting-place (stream object 'pseudo-place (symbol-name object) present inspect)
         (with-style (stream :slot-like)
           (formatting-cell (stream) (write-string "Name" stream))
           (formatting-cell (stream) (present stream)))
-        (formatting-cell (stream) (inspect stream))))
+        (formatting-cell (stream) (inspect stream)))
 
-    (formatting-row (stream)
-      (formatting-place (stream object 'pseudo-place (symbol-package object) present inspect)
+      (formatting-place (stream object 'pseudo-place (symbol-package object) present inspect) ; TODO should be mutable
         (with-style (stream :slot-like)
           (formatting-cell (stream) (write-string "Package" stream))
           (formatting-cell (stream) (present stream)))
@@ -99,16 +164,20 @@
         (with-style (stream :slot-like)
           (formatting-cell (stream) (write-string "Value" stream))
           (formatting-cell (stream) (present stream)))
-        (formatting-cell (stream) (inspect stream))))
+        (formatting-cell (stream) (inspect stream)))
 
-    (formatting-row (stream)
       (formatting-place (stream object 'symbol-function-place nil present inspect)
         (with-style (stream :slot-like)
           (formatting-cell (stream) (write-string "Function" stream))
           (formatting-cell (stream) (present stream)))
         (formatting-cell (stream) (inspect stream))))
 
-    ;; type
+    (formatting-row (stream)
+      (formatting-place (stream object 'symbol-type-place nil present inspect)
+        (with-style (stream :slot-like)
+          (formatting-cell (stream) (write-string "Type" stream))
+          (formatting-cell (stream) (present stream)))
+        (formatting-cell (stream) (inspect stream))))
     )
   ;; plist
   )
