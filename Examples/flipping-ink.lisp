699d556dd53844398f91f71808b7d7e865d44278
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-12-18 14:07:29 +0100

    examples: flipping-ink: use separate package
    Also minor tweaks to demonstrate more interactions of the flipping ink
    with the background.

--- Examples/flipping-ink.lisp
+++ Examples/flipping-ink.lisp
@@ -8,11 +8,18 @@
 ;;;
 ;;; Demonstrate the effect of flipping ink on different backgrounds.
 
-(in-package #:clim-demo)
+(defpackage #:clim-demo.flipping-ink
+  (:use
+   #:clim-lisp
+   #:clim)
+
+  (:export
+   #:flipping-ink))
+
+(in-package #:clim-demo.flipping-ink)
 
 (define-application-frame flipping-ink ()
   ()
-  ;(:geometry :min-width 100 :min-height 100)
   (:menu-bar nil)
   (:pane :application :display-function #'display
                       :width            125
@@ -21,12 +28,14 @@
 
 (defparameter *ink* (make-flipping-ink +red+ +blue+))
 
-(defmethod display ((frame flipping-ink) pane)
-  (draw-rectangle* pane 0 0 125 125 :ink +blue+)
-  (draw-text* pane "JackDaniels" 10 10 :ink *ink* :align-y :top)
-  (draw-text* pane "JackDaniels" 10 60 :ink *ink* :align-y :top)
-  ;; (draw-rectangle* pane 25 25 100 100 :ink +red+)
-  ;; (draw-rectangle* pane 35 35 90 90 :ink +black+)
-  ;; (draw-rectangle* pane 5 5 85 85 :ink *ink*)
+(defun display (frame pane)
+  (declare (ignore frame))
+  (with-bounding-rectangle* (x1 y1 x2 y2) pane
+    (draw-rectangle* pane x1 y1 x2 y2 :ink +blue+))
+  (draw-text* pane "JackDaniels" 20 10 :ink *ink* :align-y :top)
+  (draw-text* pane "JackDaniels" 20 60 :ink *ink* :align-y :top)
+  (draw-rectangle* pane 5 30 15 40 :ink +red+)
+  (draw-rectangle* pane 5 45 15 55 :ink +black+)
+  (draw-rectangle* pane 10 5 90 85 :ink *ink*)
   (draw-rectangle* pane 60 60 120 120 :ink *ink*)
   (draw-rectangle* pane 40 40 80 80 :ink *ink*))
